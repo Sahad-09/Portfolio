30f0a1924c92bef980656c51b3696e088fab6004
Sahad-09 <msahad124@gmail.com>
2024-09-30 20:24:20 +0530

    Deploy bugg

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -11,13 +11,13 @@ export const ContactSection = () => (
             backgroundImage: `url(${grainImage.src})`,
           }}
         ></div>
-        <div className=" flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+        <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
           <div>
             <h2 className="font-serif text-2xl md:text-3xl">
-              Let's create something amazing together
+              Let&apos;s create something amazing together
             </h2>
             <p className="text-sm md:text-base mt-2">
-              Ready to bring your next project to life? Let's connect and discuss
+              Ready to bring your next project to life? Let&apos;s connect and discuss
               how I can help you achieve your goals.
             </p>
           </div>
