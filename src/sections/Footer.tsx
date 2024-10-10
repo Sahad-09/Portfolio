cab5f2bd48394c5df42d8efe94b6e01d762ef4f5
Sahad-09 <msahad124@gmail.com>
2024-10-10 14:14:07 +0530

    Footer blank target

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -21,19 +21,20 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative  overflow-x-clip">
       <div
         className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-[#DF3750]/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"
       ></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
-          <div className="text-white/40">&copy; 2024. All rights reserved.</div>
-          <nav className="flex flex-col md:flex-row items-center gap-8">
+          <div className="text-white/40">&copy; 2024. All rights reserved. jb</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8 z-50">
             {footerLinks.map((link) => (
               <a
+                target="blank"
                 key={link.title}
                 href={link.href}
-                className="inline-flex items-center gap-1.5"
+                className="inline-flex items-center gap-1.5 z-60 cursor-pointer"
               >
                 <span className="font-semibold">{link.title}</span>
                 <ArrowUpRightIcon className="size-4" />
