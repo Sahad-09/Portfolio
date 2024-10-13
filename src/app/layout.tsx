2f529b680b9bfec39717c829407cd027d1c446a3
Sahad-09 <msahad124@gmail.com>
2024-10-13 12:21:46 +0530

    Pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "@/sections/Header";
 import { twMerge } from "tailwind-merge";
 import { Header } from "@/sections/Header";
+import Head from "next/head"; // Import Head
 
 const inter = Inter({
   subsets: ["latin"],
@@ -63,6 +64,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1074654170677572');
+          fbq('track', 'PageView');`
+        }} />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1074654170677572&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </Head>
       <body
         className={twMerge(
           inter.variable,
